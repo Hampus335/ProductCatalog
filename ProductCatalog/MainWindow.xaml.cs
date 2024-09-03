c47b0e705f57ded704d6ad16b1d47991a697723a
Hampus Grünewald <hampus.grunewald05@gmail.com>
2024-09-03 23:34:36 +0200

    Add project files.

---
+++ ProductCatalog/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using ProductCatalog.Pages;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductCatalog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_AddProduct(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new AddProductPage());
+        }
+
+        private void Button_ShowAllProducts(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new ShowAllProductsPage());
+        }
+
+        private void Button_SaveProducts(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_LoadProducts(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ProductCatalog/Pages/AddProductPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ProductCatalog.Products;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductCatalog
+{
+    public partial class AddProductPage : Page
+    {
+        public AddProductPage()
+        {
+            InitializeComponent();
+            CategoryDropdown.ItemsSource = Enum.GetValues(typeof(Category));
+        }
+        private void SaveProducts(object sender, RoutedEventArgs e)
+        {
+            Product product = new Product(ProductNameTextBox.Text, CategoryDropdown.SelectedItem, int.Parse(ProductPriceTextBox.Text), ProductDescriptionTextBox.Text);
+        }
+    }
+}
+    
